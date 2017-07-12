33e3a4ff0ec76af2301e91d93b0183fe4efc2fa7
HunterMcAfee <huntmac10@gmail.com>
2017-07-12 17:24:32 -0400

    Placed adding value into an array into it's own function.  Started working on a function that updates the display by looping through the array and dom manipulating the css

--- public/script/script.js
+++ public/script/script.js
@@ -10,23 +10,38 @@ var connectFour = {
         [0, 0, 0, 0, 0, 0,],
         [0, 0, 0, 0, 0, 0,],
         ],
-    checkFullColumn: function (event) {
-
-    },
-    updateGamePieceDisplay: function () {
-
-    },
-    addGamePiece: function (event) {
-        var buttonNumber = parseInt(event.target.dataset.button);
-        console.log(buttonNumber);
+    addValuetoArray: function (buttonNumber) {
         for (var i = 5; i >= 0; i--) {
             if (connectFour.gridArray[buttonNumber][i] ===  0) {
                 connectFour.gridArray[buttonNumber][i] = connectFour.playerTurn;
                 break;
             }
         }
-        updateGamePieceDisplay(buttonNumber);
-        connectFour.changePlayer();
+    },
+    checkFullColumn: function (event) {
+
+    },
+    updateGamePieceDisplay: function () {
+        console.log("I am working");
+        for (var i = 0; i < connectFour.gridArray.length; i++) {
+            for (var j = 0; j < connectFour.gridArray[i].length; j++) {
+                if (connectFour.gridArray[i][j] === 1) {
+                    var gamePieceCoordinate = '"' + '#' + i + '-' + j + '"';
+                    console.log(gamePieceCoordinate);
+                    $(gamePieceCoordinate).css("background-color", "red");
+                } else if (connectFour.gridArray[i][j] === 2) {
+                    var gamePieceCoordinate = '"' + '#' + i + '-' + j + '"';
+                    console.log(gamePieceCoordinate);
+                    $(gamePieceCoordinate).css("background-color", "black");
+            }
+        }
+        }
+    },
+    addGamePiece: function (event) {
+        var buttonNumber = parseInt(event.target.dataset.button);
+        connectFour.addValuetoArray(buttonNumber);
+        connectFour.updateGamePieceDisplay();
+        // connectFour.changePlayer();
     },
 };
 
