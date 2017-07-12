ce48c4b6e0e1a29d43a9d224d245d878dee5642a
HunterMcAfee <huntmac10@gmail.com>
2017-07-12 16:20:38 -0400

    Added button datasets that correspond to each column.  Added functionality to button that adds player value to each array.

--- public/script/script.js
+++ public/script/script.js
@@ -1,10 +1,33 @@
 
-var gridArray = [
-    [0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0,],
-];
+var connectFour = {
+    playerTurn: 1,
+    gridArray: [
+        [0, 0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0, 0,],
+        ],
+    checkFullColumn: function (event) {
+
+    },
+    updateGamePieceDisplay: function () {
+
+    },
+    addGamePiece: function (event) {
+        var buttonNumber = parseInt(event.target.dataset.button);
+        console.log(buttonNumber);
+        for (var i = 5; i >= 0; i--) {
+            if (connectFour.gridArray[buttonNumber][i] ===  0) {
+                connectFour.gridArray[buttonNumber][i] = connectFour.playerTurn;
+                break;
+            }
+        }
+        updateGamePieceDisplay(buttonNumber);
+        connectFour.changePlayer();
+    },
+};
+
+$(".buttons").click(connectFour.addGamePiece);
