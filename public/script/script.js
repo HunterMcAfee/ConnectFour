51936ecbd9d00bcf13d38bf914f09c70507f1160
HunterMcAfee <huntmac10@gmail.com>
2017-07-13 12:41:14 -0400

    Added check for horizontal function that will be caled by checkForWin

--- public/script/script.js
+++ public/script/script.js
@@ -42,51 +42,18 @@ var connectFour = {
         }
     },
     checkForWin: function () {
-        for (var i = 0; i < connectFour.gridArray.length; i++) {
+       connectFour.checkHorizontal();
+
+    },
+    checkHorizontal: function () {
+        for (var i = 0; i < 4; i++) {
             for (var j = 0; j < connectFour.gridArray[i].length; j++) {
                 if ((connectFour.gridArray[i][j] + connectFour.gridArray[i + 1][j] + connectFour.gridArray[i + 2][j] + connectFour.gridArray[i + 3][j]) === 4) {
                     console.log("Player One wins!");
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i + 1][j] + connectFour.gridArray[i + 2][j] + connectFour.gridArray[i + 3][j]) === 20) {
+                } else if ((connectFour.gridArray[i][j] + connectFour.gridArray[i + 1][j] + connectFour.gridArray[i + 2][j] + connectFour.gridArray[i + 3][j]) === 20) {
                     console.log("Player Two wins!");   
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i - 1][j] + connectFour.gridArray[i - 2][j] + connectFour.gridArray[i - 3][j]) === 4) {
-                    console.log("Player One wins!"); 
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i - 1][j] + connectFour.gridArray[i - 2][j] + connectFour.gridArray[i - 3][j]) === 20) {
-                    console.log("Player Two wins!");   
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i][j + 1] + connectFour.gridArray[i][j + 2] + connectFour.gridArray[i][j + 3]) === 4) {
-                    console.log("Player One wins!");   
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i][j + 1] + connectFour.gridArray[i][j + 2] + connectFour.gridArray[i][j + 3]) === 20) {
-                    console.log("Player Two wins!");   
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i][j - 1] + connectFour.gridArray[i][j - 2] + connectFour.gridArray[i][j - 3]) === 4) {
-                    console.log("Player One wins!");   
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i][j - 1] + connectFour.gridArray[i][j - 2] + connectFour.gridArray[i][j - 3]) === 20) {
-                    console.log("Player Two wins!");   
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i + 1][j + 1] + connectFour.gridArray[i + 2][j + 2] + connectFour.gridArray[i + 3][j + 3]) === 4) {
-                    console.log("Player One wins!");   
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i + 1][j + 1] + connectFour.gridArray[i + 2][j + 2] + connectFour.gridArray[i + 3][j + 3]) === 20) {
-                    console.log("Player Two wins!");   
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i - 1][j - 1] + connectFour.gridArray[i - 2][j - 2] + connectFour.gridArray[i - 3][j - 3]) === 4) {
-                    console.log("Player One wins!");   
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i - 1][j - 1] + connectFour.gridArray[i - 2][j - 2] + connectFour.gridArray[i - 3][j - 3]) === 20) {
-                    console.log("Player Two wins!");   
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i + 1][j - 1] + connectFour.gridArray[i + 2][j - 2] + connectFour.gridArray[i + 3][j - 3]) === 4) {
-                    console.log("Player One wins!");   
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i + 1][j - 1] + connectFour.gridArray[i + 2][j - 2] + connectFour.gridArray[i + 3][j - 3]) === 20) {
-                    console.log("Player Two wins!");   
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i - 1][j + 1] + connectFour.gridArray[i - 2][j + 2] + connectFour.gridArray[i - 3][j + 3]) === 4) {
-                    console.log("Player One wins!");   
-                } else if (connectFour.checkIfUndefined(connectFour.gridArray[i][j] + connectFour.gridArray[i - 1][j + 1] + connectFour.gridArray[i - 2][j + 2] + connectFour.gridArray[i - 3][j + 3]) === 20) {
-                    console.log("Player Two wins!");   
-                } 
+                }
             }
-        }
-    },
-    checkIfUndefined: function (sum) {
-        console.log(typeof(sum));
-        if (typeof(sum) === undefined) {
-            sum = 500;
-            return sum;
-        } else {
-            return sum;
         }
     },
     addGamePiece: function (event) {
